db54d813568054bbe373d081b0f2274bfbc84d56
Bernard Théoden <theodenbernard1203@gmail.com>
2025-04-24 17:20:33 +0200

    FEAT: add function to task 3

--- bird_count/lib/bird_count.ex
+++ bird_count/lib/bird_count.ex
@@ -5,5 +5,34 @@ defmodule BirdCount do
   def increment_day_count([]), do: [1]
 
   def increment_day_count(list), do: [(today(list) + 1)] ++ tl(list)
+
+  def has_day_without_birds?(0 = _list_a)do
+    true
+  end
+
+  def has_day_without_birds?([])do
+    false
+  end
+
+  def has_day_without_birds?(list) do 
+    
+    [head | tail] = list
+    
+    if(list == []) do
+      false
+    end
+
+    if(head == 0) do
+      
+      # has_day_without_birds?(head)
+      true
+    else
+
+      has_day_without_birds?(tail)
+
+    end
+    
+
+  end
   
 end
