7faf2db85d0888d4f529015e2a6afccc3fa55956
Bernard Théoden <theodenbernard1203@gmail.com>
2025-04-29 16:07:46 +0200

    FEAT: Secrets exercise is finished

--- secrets/lib/secrets.ex
+++ secrets/lib/secrets.ex
@@ -1,15 +1,17 @@
 defmodule Secrets do
-
-  def secret_add(secret), do: _add = fn param -> param + secret end
   
-  def secret_subtract(secret), do: _sub = fn param -> param - secret end
+  def secret_add(secret), do: fn param -> param + secret end
+  
+  def secret_subtract(secret), do: fn param -> param - secret end
 
-  def secret_multiply(secret), do: _mult = fn param -> param * secret end
+  def secret_multiply(secret), do: fn param -> param * secret end
 
-  def secret_divide(secret), do: _div = fn param -> trunc(param / secret) end
+  def secret_divide(secret), do: fn param -> trunc(param / secret) end
 
-  def secret_and(secret), do: _and = fn param -> Bitwise.band(param, secret) end
+  def secret_and(secret), do: fn param -> Bitwise.band(param, secret) end
 
-  def secret_xor(secret), do: _xor = fn param -> Bitwise.bxor(param, secret) end
+  def secret_xor(secret), do: fn param -> Bitwise.bxor(param, secret) end
+
+  def secret_combine(secret_function1, secret_function2), do: fn param -> (secret_function2.(secret_function1.(param))) end
 
 end
