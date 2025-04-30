f9e5c9dd845b2a35671049c5ada992c90fd1a20a
Bernard Théoden <theodenbernard1203@gmail.com>
2025-04-30 15:53:28 +0200

    FEAT: High score exercise is finished

--- high_score/lib/high_score.ex
+++ high_score/lib/high_score.ex
@@ -1,18 +1,19 @@
 defmodule HighScore do
-  @moduledoc """
-  Documentation for `HighScore`.
-  """
 
-  @doc """
-  Hello world.
+  @default_value 0
+    
+  def new(), do: %{}
+  
+  def add_player(scores, name, score \\ @default_value), do: Map.put(scores, name, score)
+  
+  def remove_player(scores, name), do: Map.delete(scores, name)
 
-  ## Examples
+  def reset_score(scores, name)when scores == %{}, do: add_player(scores, name, @default_value)
+  def reset_score(scores, name), do: %{scores | name => 0}
 
-      iex> HighScore.hello()
-      :world
+  def update_score(scores, name, score)when scores == %{}, do: add_player(scores, name, score)
+  def update_score(scores, name, score), do: Map.update(scores, name, @default_value, fn param -> score + param end)
 
-  """
-  def hello do
-    :world
-  end
+  def get_players(scores), do: Map.keys(scores)
+  
 end
